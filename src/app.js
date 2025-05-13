3d5bdef061fd753ff39ebf49615db5024fe70232
Jonathan0318 <jonathan0318@icloud.com>
2025-05-14 00:00:42 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["S", "H", "D", "C"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K"];
+
+    function getRandomCardURL() {
+      const value = values[Math.floor(Math.random() * values.length)];
+      const suit = suits[Math.floor(Math.random() * suits.length)];
+      return `https://deckofcardsapi.com/static/img/${value}${suit}.png`;
+    }
+
+    document.getElementById("dealButton").addEventListener("click", () => {
+      document.getElementById("cardImage").src = getRandomCardURL();
+    });
+  console.log();
 };
